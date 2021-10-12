8565acf84e2a12472377c3b2946ca59567ee9280
Leo Niemi <niemi.leo@gmail.com>
2021-10-12 18:50:04 +0300

    Tillsatt tyngdkraft

--- kodklubb.py
+++ kodklubb.py
@@ -1,32 +1,40 @@
 WIDTH = 600
 HEIGHT = 600
-
+MARK = 500
 # FÄRGER - röd, grön, blå
 # Skala 0-255
 RED = 200,0,0
 PLAYER = Rect((400, 0), (10, 10))
 HASTIGHET_Y = 0
+HASTIGHET_X = 0
+GRAVITY = 1
 
 def draw():
-    screen.draw.rect(PLAYER, RED)
+    screen.draw.filled_rect(PLAYER, RED)
 
 
 def update():
     global HASTIGHET_Y
     screen.clear()
 
-
     if keyboard.left:
-        PLAYER.x -= 1 # PLAYER.x = PLAYER.x - 1
+        PLAYER.x -= 1
 
     if keyboard.right:
         PLAYER.x += 1
 
+    if keyboard.up:
+        HASTIGHET_Y = -20
+
+
     # TYNGDKRAFT
-    if PLAYER.y < 500:
-        HASTIGHET_Y += 0.4
-        PLAYER.y += HASTIGHET_Y
+    if PLAYER.y > MARK:
+        HASTIGHET_Y = 0
+        PLAYER.y = MARK
 
-
-
+    PLAYER.y += HASTIGHET_Y
+    HASTIGHET_Y += GRAVITY
+    #if HASTIGHET_Y > 2 or HASTIGHET_Y < -2:
+    #    print(HASTIGHET_Y)
+    screen.draw.filled_rect(PLAYER, RED)
 
