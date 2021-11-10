4c51802e1059e20f7cb43fad9707e88d924ba2f0
Leo Niemi <niemi.leo@gmail.com>
2021-11-10 11:27:00 +0200

    Lade till bakgrunder, rätt main

--- kodklubb.py
+++ kodklubb.py
@@ -1,4 +1,4 @@
-WIDTH = 1200
+WIDTH = 600
 HEIGHT = 600
 MARK = 500
 # Vi sparar några färger - röd, grön, blå
@@ -12,18 +12,35 @@ BLUE = 0,0,255
 # OBS!
 # Filen måste finnas i mappen /images
 
+
+#github.com/olenleo/kodklubb21
+
 PLAYER = Actor("haffelix2",(20,20))
 HASTIGHET_Y = 0
 HASTIGHET_X = 0
 GRAVITY = 1
+ACTIVE_BLOCKS = []
+# Vi sätter in information i tabellen!
+for block in range(5):
+    nytt_block = Actor("block_red_2x2",(block * 30 + 50,10))
+    ACTIVE_BLOCKS.append(nytt_block)
+
 
 def draw():
+
+
     PLAYER.draw()
 
 
 def update():
-    global HASTIGHET_Y, HASTIGHET_X
+    global HASTIGHET_Y, HASTIGHET_X, ACTIVE_BLOCKS
+
     screen.clear()
+
+    for block in ACTIVE_BLOCKS:
+        block.y += 1
+        block.draw()
+
 
     if keyboard.left and HASTIGHET_X > -20:
         HASTIGHET_X -= 2
@@ -33,6 +50,7 @@ def update():
 
     if keyboard.up:
         HASTIGHET_Y = -10
+
 
 
     # TYNGDKRAFT
