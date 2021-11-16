1b86208dbd8cd13bbcd58d505055bbdebd727266
Leo Niemi <niemi.leo@gmail.com>
2021-11-16 22:33:59 +0200

    Mängder med kommentarer, ren kod

--- kodklubb.py
+++ kodklubb.py
@@ -1,69 +1,125 @@
+#####################################
+# Till exempel random behövs inte i så många program, därför är det inte alltid med.
+# Med 'import' bjuder vi med andra bibliotek.
+import random
+
+###################################
+# Viktiga variabler
 WIDTH = 600
 HEIGHT = 600
-MARK = 500
+
+#-----------------------------------#
+
+###################################
 # Vi sparar några färger - röd, grön, blå
 # Skala 0-255
-RED = 255,0,0
-GREEN = 0,255,0
-BLUE = 0,0,255
+red = 255, 0, 0
+green = 0, 255, 0
+blue = 0, 0, 255
+#-----------------------------------#
 
-# Skapa en actor
-# Actor("filnamn", position)
-# OBS!
-# Filen måste finnas i mappen /images
+##############################
+# OBS - dessa är *globala* variabler!
+# global hastighetX
+# Förlåt... men sånt är livet!
+hastighetY = 0
+hastighetX = 0
+gravity = 1
+activeBlocks = []
+finishedBlocks = []
+blockIsReadyToDrop = True
+#-----------------------------------#
+
+####################################
+# Skapa blocks för testbruk
+for block in range(3):
+    nyttBlock = Actor("block_red_2x2", (block * 30 + 50, 400), anchor = ('center','bottom'))
+    activeBlocks.append(nyttBlock)
+for block in range(3):
+    nyttBlock = Actor("block_red_2x2", (block * 30 + 80, 250), anchor = ('center','bottom'))
+    activeBlocks.append(nyttBlock)
+#-----------------------------------#
 
 
-#github.com/olenleo/kodklubb21
-
-PLAYER = Actor("haffelix2",(20,20))
-HASTIGHET_Y = 0
-HASTIGHET_X = 0
-GRAVITY = 1
-ACTIVE_BLOCKS = []
-# Vi sätter in information i tabellen!
-for block in range(5):
-    nytt_block = Actor("block_red_2x2",(block * 30 + 50,10))
-    ACTIVE_BLOCKS.append(nytt_block)
-
+####################################
+# draw()-loopen körs varje gång pygame ritar något
 
 def draw():
-
-
+    screen.blit("bg4", (0, 0))
     PLAYER.draw()
+    for block in activeBlocks:
+        block.draw()
+    for block in finishedBlocks:
+        # vad händer här?
+        print("Vad händer här?")
+#-----------------------------------#
 
+####################################
+# update()-loopen körs hela tiden :)
+# kom ihåg indentationen - den visar var funktionen slutar!
 
 def update():
-    global HASTIGHET_Y, HASTIGHET_X, ACTIVE_BLOCKS
-
+    global hastighetX, hastighetY, activeBlocks, finishedBlocks, blockIsReadyToDrop, readyToJump
+#### <--- indentation
     screen.clear()
 
-    for block in ACTIVE_BLOCKS:
-        block.y += 1
-        block.draw()
+    if blockIsReadyToDrop:
+        # Nästa block är inte färdigt ännu!
+        # Metod för att fälla block?
+        clock.schedule(metod, 1) # Vilken metod förbereder nästa block?
+
+    for block in activeBlocks:
+        if block.y < mark:
+            block.y += 1
+        else:
+            finishedBlocks.append(block)
+            activeBlocks.remove(block)
 
 
-    if keyboard.left and HASTIGHET_X > -20:
-        HASTIGHET_X -= 2
+    ####################################
+    # Spelarens rörelse
 
-    if keyboard.right and HASTIGHET_X < 20:
-        HASTIGHET_X += 2
+#### vi är ännu i update()...
+    if keyboard.left and hastighetX > -20:
+        hastighetX -= 2
 
-    if keyboard.up:
-        HASTIGHET_Y = -10
+    if keyboard.right and hastighetX < 20:
+        hastighetX += 2
+
+    if keyboard.up and hastighetY == 1:
+        hastighetY -= 15
+
+    #-----------------------------------#
 
 
-
+    ###############################******
     # TYNGDKRAFT
-    if PLAYER.y > MARK:
-        HASTIGHET_Y = 0
-        PLAYER.y = MARK
+    if PLAYER.y >= mark:
+        hastighetY = 0
+        PLAYER.y = mark
 
-    PLAYER.y += HASTIGHET_Y
-    PLAYER.x += HASTIGHET_X
-    HASTIGHET_Y += GRAVITY
+    PLAYER.y += hastighetY
+    PLAYER.x += hastighetX
+    hastighetY += gravity
+#### vi är ännu i update()...
+    if hastighetX > 0:
+        hastighetX -= 1
+    if hastighetX < 0:
+        hastighetX += 1
 
 
-    if HASTIGHET_X > 0:
-        HASTIGHET_X -= 1
-    if HASTIGHET_X < 0:
-        HASTIGHET_X += 1
+# men HÄR slutar update()
+#-----------------------------------#
+
+
+####################################
+# Här fäller vi block!
+def setBlockReadyToDrop():
+    global blockIsReadyToDrop
+    # True eller False?
+
+def dropABlock():
+    x = 10 # gör x till en slumpmässig siffra!
+    nyttBlock = Actor("block_red_2x2", (x, -10), anchor = ('center','bottom'))
+    # nu har vi ett nytt block... hur sparar vi den i minnet?
+#-----------------------------------#
